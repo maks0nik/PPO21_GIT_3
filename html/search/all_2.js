e1d9895feb4fec246aff74fb085a796604835dd1
Varfolomeev Maksim <varfolomeev.maks2003@mail.ru>
2024-06-05 09:18:40 +0300

    Add doc

---
+++ html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['perimetr_0',['perimetr',['../main_8cpp.html#ad026d69eaa8d5a4ae316258afc338438',1,'main.cpp']]],
+  ['ploshad_1',['ploshad',['../main_8cpp.html#a9f47c4f939b8c77080b0fee8e951ae5f',1,'main.cpp']]]
+];
---
+++ html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['input_5fa_0',['input_A',['../main_8cpp.html#a1961bd364f92cc4f85f872604295d61f',1,'main.cpp']]],
+  ['input_5fb_1',['input_B',['../main_8cpp.html#a35c3173ff9561a82d5051e3380e3e3b2',1,'main.cpp']]]
+];
